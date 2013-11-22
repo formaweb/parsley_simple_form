7519f90fd131d4f34fd0da02d0644b10822429af
Matheus Caceres <matheus@formaweb.com.br>
2013-11-22 13:39:14 -0200

    Add requires libs

--- lib/parsley_simple_form.rb
+++ lib/parsley_simple_form.rb
@@ -1,5 +1,8 @@
-require "parsley_simple_form/version"
+require 'parsley_simple_form/version'
+require 'simple_form_extensions/form_helper'
+require 'parsley_simple_form/simple_form_adapt'
+
+require 'simple_form'
 
 module ParsleySimpleForm
-  # Your code goes here...
 end
