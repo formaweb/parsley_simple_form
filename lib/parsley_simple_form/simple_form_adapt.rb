11a17e24f3736aa5d3c5eb2a76df12e279a6b7d3
Matheus Caceres <matheuscaceres@gmail.com>
2013-11-25 16:58:45 -0200

    Change method _validators to validators_on; Add SimpleFormAdapt#object_class

--- lib/parsley_simple_form/simple_form_adapt.rb
+++ lib/parsley_simple_form/simple_form_adapt.rb
@@ -12,7 +12,7 @@ module ParsleySimpleForm
     private
 
     def validations_for(attribute_name)
-      object._validators[attribute_name].each_with_object({}) do |validate, attributes|
+      object_class.validators_on(attribute_name).each_with_object({}) do |validate, attributes|
         next unless klass = validate_constantize(validate.kind)
 
         attributes.merge!(klass.new(validate).attribute_validate)
@@ -21,6 +21,10 @@ module ParsleySimpleForm
 
     def validate_constantize(validate_kind)
       ('parsley_simple_form/validators/' + validate_kind.to_s).camelize.constantize rescue false
+    end
+
+    def object_class
+      object.class
     end
   end
 end
