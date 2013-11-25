200ed13b738e0529a11a099a51d386e05c13b8c4
Matheus Caceres <matheuscaceres@gmail.com>
2013-11-25 15:46:02 -0200

    Fixed message test

--- test/parsley_simple_form/simple_form_adapt_test.rb
+++ test/parsley_simple_form/simple_form_adapt_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class SimpleFormAdaptTest < ActionView::TestCase
-  test "add attribute require when model has presence validation" do
+  test "verify whether helper is a instance of ParsleySimpleForm::SimpleFormAdapt" do
     parsley_concat_form(@company) do |f|
       assert f.instance_of?(ParsleySimpleForm::SimpleFormAdapt)
     end
