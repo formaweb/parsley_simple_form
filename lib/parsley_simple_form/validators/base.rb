41aaef59abb3fb21464f1da0e3e7e0a26fcd4973
Matheus Caceres <matheuscaceres@gmail.com>
2013-11-25 17:01:29 -0200

    Changes Validators::Base#new, now it receive 3 params; Add ability to generate message error;

--- lib/parsley_simple_form/validators/base.rb
+++ lib/parsley_simple_form/validators/base.rb
@@ -2,13 +2,25 @@ module ParsleySimpleForm
   module Validators
 
     class Base
-      attr_reader :validate, :options
+      attr_reader :object, :error, :validate, :options, :attribute_name
 
-      def initialize(validate)
+      def initialize(object, validate, attribute_name)
+        @object = object
+        @error = object.errors
         @validate = validate
-        @options = validate.options
+        @options = validate.options.dup
+        @attribute_name = attribute_name
       end
 
+      def message_error
+        generate_message
+      end
+
+      protected
+
+      def generate_message
+        error.generate_message(attribute_name, validate.kind, options)
+      end
     end
 
   end
