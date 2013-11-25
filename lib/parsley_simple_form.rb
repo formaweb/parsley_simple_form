98195255c82e6566f7ae0966fbf690ce7f9b9f56
Matheus Caceres <matheus@formaweb.com.br>
2013-11-25 15:27:02 -0200

    Add simple_form dependecy; Add boolean attributes for parsley

--- lib/parsley_simple_form.rb
+++ lib/parsley_simple_form.rb
@@ -1,8 +1,18 @@
-require 'parsley_simple_form/version'
-require 'simple_form_extensions/form_helper'
-require 'parsley_simple_form/simple_form_adapt'
-
 require 'simple_form'
 
+require 'parsley_simple_form/version'
+require 'parsley_simple_form/simple_form_extensions/form_helper'
+require 'parsley_simple_form/validators'
+require 'parsley_simple_form/simple_form_adapt'
+
 module ParsleySimpleForm
+  extend ActiveSupport::Autoload
+  @@parsley_attributes = [
+    "parsley-validate",
+    "parsley-required",
+  ]
+
+  ActionView::Helpers::TagHelper::BOOLEAN_ATTRIBUTES << Set.new(@@parsley_attributes)
+
+  ActionView::Helpers::TagHelper::BOOLEAN_ATTRIBUTES.flatten!
 end
