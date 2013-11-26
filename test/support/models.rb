b10c9ceea99dbfdccfcf322f926fea02514d3e07
Matheus Caceres <matheuscaceres@gmail.com>
2013-11-26 14:39:13 -0200

    Add more validation to test

--- test/support/models.rb
+++ test/support/models.rb
@@ -4,7 +4,8 @@ class Company
   attr_accessor :name, :address, :number
 
   validates :name, :address, :number, presence: {message: "cannot be blank!"}
-  validates :name, :address, length: { maximum: 100 }
+  validates :name, length: {in: 6..20}
+  validates :address, length: {minimum: 10, maximum: 100, too_long: "100 characters is the maximum allowed", too_short: "10 characters is the minimum allowed"}
   validates :number, numericality: true
 
   def column_for_attribute(attribute_name)
