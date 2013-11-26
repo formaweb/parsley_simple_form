ac3232b1a6f21fd96d9eb4a12104e9241552f521
Matheus Caceres <matheuscaceres@gmail.com>
2013-11-26 16:00:05 -0200

    Add documentation

--- lib/parsley_simple_form/simple_form_extensions/form_helper.rb
+++ lib/parsley_simple_form/simple_form_extensions/form_helper.rb
@@ -4,6 +4,7 @@ module ParsleySimpleForm
     module FormHelper
       include SimpleForm::ActionViewExtensions::FormHelper
 
+      # Custom helper to SimpleForm
       def parsley_simple_for(resource, *args, &block)
         options = args.extract_options!
         simple_form_for(resource, *(args << options.merge(builder: ParsleySimpleForm::SimpleFormAdapt, html: {:"parsley-validate" => true})), &block)
