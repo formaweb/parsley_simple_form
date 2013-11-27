720b470b9e52c75047e6436801a4ccde776e0001
Matheus Caceres <matheuscaceres@gmail.com>
2013-11-27 15:14:38 -0200

    Add more numericality validation

--- test/support/models.rb
+++ test/support/models.rb
@@ -1,19 +1,26 @@
+Column = Struct.new(:type, :limit) do
+  def number?
+    type == (:integer || :float || :decimal)
+  end
+end
 class Company
   include ActiveModel::Model
 
-  attr_accessor :name, :address, :number
+  attr_accessor :name, :address, :number, :number_only_integer
 
   validates :name, :address, :number, presence: {message: "cannot be blank!"}
   validates :name, length: {in: 6..20}
   validates :address, length: {minimum: 10, maximum: 100, too_long: "100 characters is the maximum allowed", too_short: "10 characters is the minimum allowed"}
-  validates :number, numericality: true
+  validates :number, numericality: {greater_than_or_equal_to: 6, less_than_or_equal_to: 100, message: "is an invalid number."}
+  validates :number_only_integer, numericality: {only_integer: true, message: "is an invalid number."}
 
   def column_for_attribute(attribute_name)
     column = case attribute_name.to_sym
-    when :name then :string
-    when :address then :string
-    when :number then :integer
+      when :name then :string
+      when :address then :string
+      when :number then :integer
     end
+    Column.new(column)
   end
 
   def model_name
