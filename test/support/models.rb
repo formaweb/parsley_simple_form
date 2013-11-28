a48fca8058c9ddaf3e77fedf6fbab51982b564d4
Matheus Caceres <matheuscaceres@gmail.com>
2013-11-28 10:38:27 -0200

    Update Company model test to phone_kind, validated with inclusion

--- test/support/models.rb
+++ test/support/models.rb
@@ -6,19 +6,21 @@ end
 class Company
   include ActiveModel::Model
 
-  attr_accessor :name, :address, :number, :number_only_integer
+  attr_accessor :name, :address, :number, :number_only_integer, :phone_kind
 
   validates :name, :address, :number, presence: {message: "cannot be blank!"}
   validates :name, length: {in: 6..20}
   validates :address, length: {minimum: 10, maximum: 100, too_long: "100 characters is the maximum allowed", too_short: "10 characters is the minimum allowed"}
   validates :number, numericality: {greater_than_or_equal_to: 6, less_than_or_equal_to: 100, message: "is an invalid number."}
   validates :number_only_integer, numericality: {only_integer: true, message: "is an invalid number."}
+  validates :phone_kind, inclusion: {in: %w(phone cell-phone), message: "is not included in the list"}
 
   def column_for_attribute(attribute_name)
     column = case attribute_name.to_sym
       when :name then :string
       when :address then :string
       when :number then :integer
+      when :phone_kind then :string
     end
     Column.new(column)
   end
